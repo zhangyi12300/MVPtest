d0cb6e5c82b7a3a8d37a40ff2dbec4308d2237fe
zhangyi12300 <1544536395@qq.con>
2017-06-22 13:58:20 +0800

    test

---
+++ app/src/main/java/com/example/zhangyi/mvptest/newTestActivity.java
@@ -0,0 +1,17 @@
+package com.example.zhangyi.mvptest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhangyi on 2017/6/22.
+ */
+
+public class newTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
