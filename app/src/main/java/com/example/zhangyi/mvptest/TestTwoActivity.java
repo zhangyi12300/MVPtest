39caaf9fac920db59b2185cfae36b6689c87fba1
zhangyi12300 <1544536395@qq.con>
2017-06-22 14:12:31 +0800

    新的测试

--- app/src/main/java/com/example/zhangyi/mvptest/TestTwoActivity.java
+++ app/src/main/java/com/example/zhangyi/mvptest/TestTwoActivity.java
@@ -7,11 +7,11 @@ import android.os.Bundle;
  * Created by zhangyi on 2017/6/22.
  */
 
-public class newTestActivity extends Activity {
+public class TestTwoActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //新的测试  测试新数据
+
     }
 }
