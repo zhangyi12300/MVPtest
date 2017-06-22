df5b3d5ee8f2767de731ee7b9315ad417267fb50
zhangyi12300 <1544536395@qq.con>
2017-06-22 15:00:14 +0800

    333

--- app/src/main/java/com/example/zhangyi/mvptest/TestThreeActivity.java
+++ app/src/main/java/com/example/zhangyi/mvptest/TestThreeActivity.java
@@ -1,17 +1,34 @@
 package com.example.zhangyi.mvptest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by zhangyi on 2017/6/22.
  */
 
-public class TestThreeActivity extends Activity{
+public class TestThreeActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        
     }
 }
