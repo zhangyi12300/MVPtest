e3039fbb3cc9431487d9f215dd0915b76c68c42f
zhangyi12300 <1544536395@qq.con>
2017-06-22 14:50:49 +0800

    222

--- app/src/main/java/com/example/zhangyi/mvptest/TestThreeActivity.java
+++ app/src/main/java/com/example/zhangyi/mvptest/TestThreeActivity.java
@@ -12,6 +12,6 @@ public class TestThreeActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
     }
 }
